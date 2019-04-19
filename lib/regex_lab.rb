d2e5c33d0cb64787b3d1cebcbefe61ebdfbe032d
Meg Galvez <meg@oakmaine.com>
2019-04-19 16:25:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]\z\W/) == 
+  if text.match(/\b[A-Z]\W\b/) == nil
+    false
+  else 
+    true
+  end 
 end
 
 def valid_phone_number?(phone)
