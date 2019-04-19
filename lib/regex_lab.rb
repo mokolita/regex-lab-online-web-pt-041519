de6e15f6acf1c554b14b92cc3f477256a26cd364
Meg Galvez <meg@oakmaine.com>
2019-04-19 16:14:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  array = []
-  if text.match(/\A[un]{2}\z[ing]/) 
-    array << text 
-  end 
-  array 
+ text.scan(//)
 end
 
 def words_five_letters_long(text)
