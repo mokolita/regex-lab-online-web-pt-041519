bc4ee95f18701b0dedb6ceb4103cf6cf368ae2aa
Meg Galvez <meg@oakmaine.com>
2019-04-19 16:29:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\b\d{3}/)
+  if phone.match(/\b\d{3}\W\d{3}\W\d{4}/) == nil
+    false
+  else 
+    true
+  end 
 end
